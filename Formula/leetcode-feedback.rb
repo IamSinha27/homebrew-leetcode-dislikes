a96be3b190763446f3688f7e24bd78b6ab667bc5
IamSinha27 <ssinha_be20@thapar.edu>
2024-07-10 00:04:28 +0530

    :Added shasum key

--- Formula/leetcode-feedback.rb
+++ Formula/leetcode-feedback.rb
@@ -2,7 +2,7 @@ class LeetcodeFeedback < Formula
   desc "A script to fetch the likes, dislikes, and dislike percentage of a LeetCode problem"
   homepage "https://github.com/IamSinha27/homebrew-leetcode-dislikes"
   url "https://github.com/yourusername/homebrew-leetcode-feedback/archive/v1.0.tar.gz"
-  sha256 "your_tarball_sha256"
+  sha256 "125139bd15dd937a1763f8a6eb60b09fd93eaa87a6a9ea063fc0233406555af1"
   version "1.0"
 
   def install
