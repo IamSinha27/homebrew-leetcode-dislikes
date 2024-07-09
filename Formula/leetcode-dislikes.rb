c59840f6d272bec44bf9ef6a20ea2ba06ae0eb43
IamSinha27 <ssinha_be20@thapar.edu>
2024-07-10 00:25:29 +0530

    Renaming formula

--- Formula/leetcode-dislikes.rb
+++ Formula/leetcode-dislikes.rb
@@ -1,7 +1,7 @@
 class LeetcodeDislikes < Formula
   desc "A script to fetch the likes, dislikes, and dislike percentage of a LeetCode problem"
   homepage "https://github.com/IamSinha27/homebrew-leetcode-dislikes"
-  url "https://github.com/yourusername/homebrew-leetcode-dislikes/archive/v1.0.tar.gz"
+  url "https://github.com/IamSinha27/homebrew-leetcode-dislikes/archive/v1.0.tar.gz"
   sha256 "125139bd15dd937a1763f8a6eb60b09fd93eaa87a6a9ea063fc0233406555af1"
   version "1.0"
 
