726507f8fa53c39be12b0f722930150d7e08493b
IamSinha27 <ssinha_be20@thapar.edu>
2024-07-10 01:49:37 +0530

    New version

--- Formula/leetcode-dislikes.rb
+++ Formula/leetcode-dislikes.rb
@@ -1,9 +1,9 @@
 class LeetcodeDislikes < Formula
   desc "A script to fetch the likes, dislikes, and dislike percentage of a LeetCode problem"
   homepage "https://github.com/IamSinha27/homebrew-leetcode-dislikes"
-  url "https://github.com/IamSinha27/homebrew-leetcode-dislikes/archive/v1.1.tar.gz"
-  sha256 "18e1e924f8010f4c6b46f4b626fc4b16abaa86c65d83d954832d8f5bc1e960be"
-  version "1.1"
+  url "https://github.com/IamSinha27/homebrew-leetcode-dislikes/archive/v1.2.tar.gz"
+  sha256 "3025f1421261bdbf2a8e43abcbc9322567218d5f1a48a5d7e8210f1d199c6e65"
+  version "1.2"
 
   def install
     bin.install "leetcode-dislikes.sh" => "leetcode-dislikes"
